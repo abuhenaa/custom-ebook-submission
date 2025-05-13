4a771897e2a2259610cac193bbbbeeaee28ee0ec
abuhenaa <abuhena.ict@gmail.com>
2025-05-14 02:06:40 +0600

    init

---
+++ custom-ebook-submission.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Custom Ebook Submission
+Description: A custom plugin to handle ebook submissions.
+Version: 1.0
+Author: Abu Hena
+Author URI: https://www.example.com
+License: GPL2
+Text Domain: custom-ebook-submission
+*/
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
